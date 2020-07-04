36ab0524d5667abf18c1735ef0c0714b63bdc93b
Sanket Landage <sanket.landage@gmail.com>
2020-07-05 01:20:30 +0530

    Updates

---
+++ precache-manifest.5ea355efa3d4e1dcffddc6ed05c82cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d76c29bc1692b8d993d604b46150161",
+    "url": "/React-App-github-finder/index.html"
+  },
+  {
+    "revision": "cc581334485637e0fae9",
+    "url": "/React-App-github-finder/static/css/main.5d022ffa.chunk.css"
+  },
+  {
+    "revision": "aad6ef799954dfb1f0f3",
+    "url": "/React-App-github-finder/static/js/2.a0c75083.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-App-github-finder/static/js/2.a0c75083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc581334485637e0fae9",
+    "url": "/React-App-github-finder/static/js/main.c64205f1.chunk.js"
+  },
+  {
+    "revision": "51cbea96306ac01f090c",
+    "url": "/React-App-github-finder/static/js/runtime-main.65d7934f.js"
+  }
+]);
